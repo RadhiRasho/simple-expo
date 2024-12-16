5c38bd2166fc274e496a0cef94bcb0fb8c61a179
Radhi Rasho <Radhi.h@outlook.com>
2024-12-16 21:02:11 +0000

    batman

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,92 +1,31 @@
-import { Link } from "expo-router";
-import React from "react";
-import { Text, View } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { type CameraType, CameraView } from "expo-camera";
+import React, { useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Page() {
-  return (
-    <View className="flex flex-1">
-      <Header />
-      <Content />
-      <Footer />
-    </View>
-  );
+	return (
+		<View className="flex flex-1">
+			<Content />
+		</View>
+	);
 }
 
 function Content() {
-  return (
-			<View className="flex-1">
-				<View className="py-12 md:py-24 lg:py-32 xl:py-48">
-					<View className="px-4 md:px-6">
-						<View className="flex flex-col items-center gap-4 text-center">
-							<Text className="text-6xl font-bold">
-								Welcome to Project ACME
-							</Text>
-							<Text className="mx-auto max-w-[700px] text-lg text-center text-gray-500 md:text-xl dark:text-gray-400">
-								Discover and collaborate on acme. Explore our services now.
-							</Text>
+	const [facing, setFacing] = useState<CameraType>("front");
 
-							<View className="gap-4">
-								<Link
-									suppressHighlighting
-									className="flex h-9 items-center justify-center overflow-hidden rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 web:shadow ios:shadow transition-colors hover:bg-gray-900/90 active:bg-gray-400/90 web:focus-visible:outline-none web:focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-									href="/"
-								>
-									Explore
-								</Link>
-							</View>
-						</View>
-					</View>
+	function flip() {
+		setFacing(facing === "front" ? "back" : "front");
+	}
+
+	return (
+		<View className="flex flex-1">
+			<CameraView className="flex-1 flex" facing={facing}>
+				<View className="absolute bottom-0 left-0 right-0 p-4 flex flex-row justify-center">
+					<TouchableOpacity className="" onPress={flip}>
+						<Text className="text-foreground">Flip Camera</Text>
+					</TouchableOpacity>
 				</View>
-			</View>
-		);
-}
-
-function Header() {
-  const { top } = useSafeAreaInsets();
-  return (
-    <View style={{ paddingTop: top }}>
-      <View className="px-4 lg:px-6 h-14 flex items-center flex-row justify-between ">
-        <Link className="font-bold flex-1 items-center justify-center" href="/">
-          ACME
-        </Link>
-        <View className="flex flex-row gap-4 sm:gap-6">
-          <Link
-            className="text-md font-medium hover:underline web:underline-offset-4"
-            href="/"
-          >
-            About
-          </Link>
-          <Link
-            className="text-md font-medium hover:underline web:underline-offset-4"
-            href="/"
-          >
-            Product
-          </Link>
-          <Link
-            className="text-md font-medium hover:underline web:underline-offset-4"
-            href="/"
-          >
-            Pricing
-          </Link>
-        </View>
-      </View>
-    </View>
-  );
-}
-
-function Footer() {
-  const { bottom } = useSafeAreaInsets();
-  return (
-    <View
-      className="flex shrink-0 bg-gray-100 native:hidden"
-      style={{ paddingBottom: bottom }}
-    >
-      <View className="py-6 flex-1 items-start px-4 md:px-6 ">
-        <Text className={"text-center text-gray-700"}>
-          © {new Date().getFullYear()} Me
-        </Text>
-      </View>
-    </View>
-  );
+			</CameraView>
+		</View>
+	);
 }
