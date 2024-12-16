e107c1269cd07e1a41edf7c9d4879606ba69c933
Radhi Rasho <Radhi.h@outlook.com>
2024-12-16 19:00:29 +0000

    batman

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "tailwindcss";
+
+export default {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	presets: [require("nativewind/preset")],
+	theme: {
+		extend: {},
+	},
+	future: {
+		hoverOnlyWhenSupported: true,
+	},
+	plugins: [],
+} satisfies Config;
